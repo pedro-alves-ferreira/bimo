25789fd18789d3f289ea3b5c537c50b090de8c65
Pedro Ferreira <pedro@bisect.pt>
2018-07-31 17:35:17 +0200

    Removed telemetry stuff

--- cpp/lib/src/memory/telemetry.cpp
+++ cpp/lib/src/memory/telemetry.cpp
@@ -1,34 +1,6 @@
 #include "bisect/bimo/memory/telemetry.h"
 using namespace bisect::bimo;
 
-//#define EBU_LIST_ENABLE_TELEMETRY
-
-//------------------------------------------------------------------------------
-void allocation_telemetry::dump()
-{
-    printf("Allocations: %d\n", counter_.load());
-
-    for (const auto[size, count] : histogram_)
-    {
-        printf("Size: %lld\tCount: %d\n", size, count);
-    }
-}
-
-void allocation_telemetry::on_allocation(size_t _size)
-{
-    ++counter_;
-
-    const auto it = histogram_.find(_size);
-    if (it == histogram_.end())
-    {
-        histogram_.insert({ _size, 1 });
-    }
-    else
-    {
-        const auto v = it->second + 1;
-        histogram_.insert_or_assign(_size, v);
-    }
-}
 //------------------------------------------------------------------------------
 
 telemetry& telemetry::instance()
@@ -36,28 +8,6 @@ telemetry& telemetry::instance()
     static telemetry instance;
     return instance;
 }
-
-#if defined(EBU_LIST_ENABLE_TELEMETRY)
-
-telemetry::~telemetry()
-{
-    printf("Direct allocation:\n");
-    direct_allocation_.dump();
-    printf("Allocation through factory:\n");
-    allocation_through_factory_.dump();
-}
-
-void telemetry::on_direct_allocation(size_t _size)
-{
-    direct_allocation_.on_allocation(_size);
-}
-
-void telemetry::on_allocation_through_factory(size_t _size)
-{
-    allocation_through_factory_.on_allocation(_size);
-}
-
-#else // defined(EBU_LIST_ENABLE_TELEMETRY)
 
 telemetry::~telemetry()
 {
@@ -70,6 +20,4 @@ void telemetry::on_direct_allocation(size_t /*_size*/)
 void telemetry::on_allocation_through_factory(size_t /*_size*/)
 {
 }
-
-#endif // defined(EBU_LIST_ENABLE_TELEMETRY)
 
