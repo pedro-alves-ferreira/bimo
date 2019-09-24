cc2646f83588b795e1f38d315bec3305dee7a5c5
Pedro Ferreira <pedro@bisect.pt>
2019-09-24 11:43:18 +0100

    Cleanup Cmake stuff

--- config/cmake/cpp-requirements.cmake
+++ config/cmake/cpp-requirements.cmake
@@ -5,8 +5,8 @@ function(bimo_set_cpp_properties)
         CHECK_CXX_COMPILER_FLAG("/std:c++17" _cpp_latest_flag_supported)
         if (_cpp_latest_flag_supported)
             add_compile_options("/std:c++17")
-        endif()
-    endif()
+        endif ()
+    endif ()
 endfunction(bimo_set_cpp_properties)
 
 macro(bimo_clear_default_cpp_properties)
@@ -15,7 +15,7 @@ macro(bimo_clear_default_cpp_properties)
     elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
     elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
         bimo_clear_default_msvc_cpp_properties()
-    endif()
+    endif ()
 
     set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} PARENT_SCOPE)
 endmacro()
@@ -23,23 +23,31 @@ endmacro()
 macro(bimo_clear_default_msvc_cpp_properties)
     # Force to always compile with W4
     set(LOCAL_CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
-    if(LOCAL_CMAKE_CXX_FLAGS MATCHES "/WX- ")
+    if (LOCAL_CMAKE_CXX_FLAGS MATCHES "/WX- ")
         string(REGEX REPLACE "/WX-" LOCAL_CMAKE_CXX_FLAGS "${LOCAL_CMAKE_CXX_FLAGS}")
-    endif()
-    if(LOCAL_CMAKE_CXX_FLAGS MATCHES "/W[0-4]")
+    endif ()
+    if (LOCAL_CMAKE_CXX_FLAGS MATCHES "/W[0-4]")
         string(REGEX REPLACE "/W[0-4] " "" LOCAL_CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
-    endif()
+    endif ()
 
     set(LOCAL_CMAKE_CXX_FLAGS "${LOCAL_CMAKE_CXX_FLAGS}" PARENT_SCOPE)
     set(CMAKE_CXX_FLAGS "${LOCAL_CMAKE_CXX_FLAGS}" PARENT_SCOPE)
 endmacro()
 
+macro(bimo_set_target_c_properties_no_warnings)
+    if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+        bimo_set_target_gcc_c_properties_no_warnings()
+    endif ()
+endmacro()
+
 macro(bimo_set_target_cpp_properties)
     set_target_properties(${PROJECT_NAME} PROPERTIES
-        CXX_STANDARD 17
-        CXX_STANDARD_REQUIRED YES
-        CXX_EXTENSIONS NO
-        )
+            CXX_STANDARD 17
+            CXX_STANDARD_REQUIRED YES
+            CXX_EXTENSIONS NO
+            )
+
+    target_compile_features(${PROJECT_NAME} PUBLIC cxx_std_17)
 
     if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang") # Clang and AppleClang
     elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
@@ -47,15 +55,30 @@ macro(bimo_set_target_cpp_properties)
     elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
     elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
         bimo_set_target_msvc_cpp_properties()
-    endif()
+    endif ()
 
 endmacro()
 
+function(bimo_set_target_gcc_c_properties_no_warnings)
+    set(MY_DEBUG_OPTIONS -O0 -g)
+    target_compile_options(${PROJECT_NAME} PRIVATE "$<$<CONFIG:DEBUG>:${MY_DEBUG_OPTIONS}>")
+endfunction()
+
 function(bimo_set_target_gcc_cpp_properties)
-    target_compile_options(${PROJECT_NAME} PRIVATE -Wall -Wextra -Wshadow -Wnon-virtual-dtor
+    target_compile_options(${PROJECT_NAME} PRIVATE -Wall -Wextra -Wshadow
             -Wduplicated-cond -Wduplicated-branches -Wlogical-op -Wrestrict -Wnull-dereference
-            -Wold-style-cast -Wno-useless-cast -Wdouble-promotion -Wno-unknown-pragmas
+            -Wdouble-promotion -Wno-unknown-pragmas
             -Wformat=2 -pedantic)
+
+    if (NOT ${NO_WARNINGS_AS_ERRORS})
+        target_compile_options(${PROJECT_NAME} PRIVATE -Werror)
+    endif ()
+
+    set(MY_CXX_OPTIONS -Wnon-virtual-dtor -Wold-style-cast -Wno-useless-cast)
+    target_compile_options(${PROJECT_NAME} PRIVATE "$<$<COMPILE_LANGUAGE:CXX>:${MY_CXX_OPTIONS}>")
+
+    set(MY_DEBUG_OPTIONS -O0 -g)
+    target_compile_options(${PROJECT_NAME} PRIVATE "$<$<CONFIG:DEBUG>:${MY_DEBUG_OPTIONS}>")
 endfunction()
 
 function(bimo_set_target_msvc_cpp_properties)
